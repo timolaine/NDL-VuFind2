efb9487ecda9d776f80c5102e17869920181b8ff
Timo Laine <timo.mz.laine@helsinki.fi>
2016-07-01 10:38:38 +0300

    Allow both lowercase and uppercase forms of "IMAGE" in MARC 856q for thumbnail images.

--- module/Finna/src/Finna/RecordDriver/SolrMarc.php
+++ module/Finna/src/Finna/RecordDriver/SolrMarc.php
@@ -209,7 +209,7 @@ class SolrMarc extends \VuFind\RecordDriver\SolrMarc
             $type = $url->getSubfield('q');
             if ($type) {
                 $type = $type->getData();
-                if ("IMAGE" == $type || "image/jpeg" == $type) {
+                if ("image" == strtolower($type) || "image/jpeg" == $type) {
                     $address = $url->getSubfield('u');
                     if ($address && $this->urlAllowed($address->getData())) {
                         $address = $address->getData();
